2cf5504b6dd9def10cfa7454ee301686fb045ebc
ZOAZHYGA <zoazhyga@ZOAZHYGA.local>
2020-01-28 16:54:09 +0100

    Create main file

---
+++ run_train.py
@@ -0,0 +1,10 @@
+from __future__ import absolute_import, print_function, division, unicode_literals
+
+import tensorflow as tf
+
+def main():
+    return 0
+
+
+if __name__ == '__main__':
+    main()
